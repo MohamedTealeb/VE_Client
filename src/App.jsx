85dc7bfa42fcd2db9d82c08402598cf4b69b1467
MohamedTealeb <mohamedtealeb088@gmail.com>
2025-05-13 19:49:15 +0300

    cart page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./Modules/Home/Home";
 import SignUp from "./Modules/Auth/Signup/SignUp";
 import Product from "./Modules/Product/Product";
 import Product_Det from "./Modules/Product/Product_Det";
+import Cart from "./Modules/Cart/Cart";
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
       <Route path="signup" element={<SignUp />} />
       <Route path="product" element={<Product />} />
       <Route path="product_det" element={<Product_Det />} />
+      <Route path="cart" element={<Cart />}/>
      </Routes>
     </>
   )
