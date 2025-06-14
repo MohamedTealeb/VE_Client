16c267c564eb81d65d064b49d18fa49def387bbe
MohamedTealeb <mohamedtealeb088@gmail.com>
2025-06-14 22:45:17 +0300

    gh

--- src/Modules/Category/Category.jsx
+++ src/Modules/Category/Category.jsx
@@ -29,7 +29,8 @@ const Category = () => {
 
   const handleExploreCategory = (categoryId) => {
     navigate(`/product`);
-  };
+  }; 
+  
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white flex flex-col">
